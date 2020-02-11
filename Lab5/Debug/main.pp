98c6b18e596552a6ce170cb14500c51c1144d739
Rahul <rahul.iyer2@gmail.com>
2020-02-11 13:09:30 -0800

    lab5...training data collected

--- Lab5/Debug/main.pp
+++ Lab5/Debug/main.pp
@@ -34,6 +34,88 @@ main.obj: ../hw_types.h
 main.obj: ../lcdkOMAPL138.h
 main.obj: ../soc_OMAPL138.h
 main.obj: ../hw_syscfg0_OMAPL138.h
+main.obj: ../hamming_constants.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/dsplib.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap16/DSPF_blk_eswap16.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap16/c674/DSPF_blk_eswap16.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap32/DSPF_blk_eswap32.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap32/c674/DSPF_blk_eswap32.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap64/DSPF_blk_eswap64.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap64/c674/DSPF_blk_eswap64.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_fltoq15/DSPF_fltoq15.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_fltoq15/c674/DSPF_fltoq15.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_q15tofl/DSPF_q15tofl.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_q15tofl/c674/DSPF_q15tofl.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_autocor/DSPF_sp_autocor.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_autocor/c674/DSPF_sp_autocor.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_biquad/DSPF_sp_biquad.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_biquad/c674/DSPF_sp_biquad.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_bitrev_cplx/DSPF_sp_bitrev_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_bitrev_cplx/c674/DSPF_sp_bitrev_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_blk_move/DSPF_sp_blk_move.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_blk_move/c674/DSPF_sp_blk_move.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr2_dit/DSPF_sp_cfftr2_dit.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr2_dit/c674/DSPF_sp_cfftr2_dit.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr4_dif/DSPF_sp_cfftr4_dif.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr4_dif/c674/DSPF_sp_cfftr4_dif.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_convol/DSPF_sp_convol.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_convol/c674/DSPF_sp_convol.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotprod/DSPF_sp_dotprod.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotprod/c674/DSPF_sp_dotprod.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotp_cplx/DSPF_sp_dotp_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotp_cplx/c674/DSPF_sp_dotp_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fftSPxSP/DSPF_sp_fftSPxSP.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fftSPxSP/c674/DSPF_sp_fftSPxSP.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fircirc/DSPF_sp_fircirc.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fircirc/c674/DSPF_sp_fircirc.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_cplx/DSPF_sp_fir_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_cplx/c674/DSPF_sp_fir_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_gen/DSPF_sp_fir_gen.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_gen/c674/DSPF_sp_fir_gen.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_r2/DSPF_sp_fir_r2.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_r2/c674/DSPF_sp_fir_r2.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_icfftr2_dif/DSPF_sp_icfftr2_dif.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_icfftr2_dif/c674/DSPF_sp_icfftr2_dif.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_ifftSPxSP/DSPF_sp_ifftSPxSP.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_ifftSPxSP/c674/DSPF_sp_ifftSPxSP.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iirlat/DSPF_sp_iirlat.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iirlat/c674/DSPF_sp_iirlat.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iir/DSPF_sp_iir.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iir/c674/DSPF_sp_iir.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_lms/DSPF_sp_lms.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_lms/c674/DSPF_sp_lms.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul/DSPF_sp_mat_mul.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul/c674/DSPF_sp_mat_mul.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_cplx/DSPF_sp_mat_mul_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_cplx/c674/DSPF_sp_mat_mul_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm/DSPF_sp_mat_mul_gemm.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm/c674/DSPF_sp_mat_mul_gemm.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm_cplx/DSPF_sp_mat_mul_gemm_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm_cplx/c674/DSPF_sp_mat_mul_gemm_cplx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_trans/DSPF_sp_mat_trans.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_trans/c674/DSPF_sp_mat_trans.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxidx/DSPF_sp_maxidx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxidx/c674/DSPF_sp_maxidx.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxval/DSPF_sp_maxval.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxval/c674/DSPF_sp_maxval.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minerr/DSPF_sp_minerr.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minerr/c674/DSPF_sp_minerr.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minval/DSPF_sp_minval.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minval/c674/DSPF_sp_minval.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecmul/DSPF_sp_vecmul.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecmul/c674/DSPF_sp_vecmul.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecrecip/DSPF_sp_vecrecip.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecrecip/c674/DSPF_sp_vecrecip.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecsum_sq/DSPF_sp_vecsum_sq.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecsum_sq/c674/DSPF_sp_vecsum_sq.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_w_vec/DSPF_sp_w_vec.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_w_vec/c674/DSPF_sp_w_vec.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h16/DSP_fir_r8_h16.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h16/c674/DSP_fir_r8_h16.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h24/DSP_fir_r8_h24.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h24/c674/DSP_fir_r8_h24.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h8/DSP_fir_r8_h8.h
+main.obj: C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h8/c674/DSP_fir_r8_h8.h
 
 ../main.c: 
 ../L138_LCDK_aic3106_init.h: 
@@ -69,3 +151,85 @@ C:/ti/OMAPL138_StarterWare_1_10_04_01/include/hw/hw_psc_OMAPL138.h:
 ../lcdkOMAPL138.h: 
 ../soc_OMAPL138.h: 
 ../hw_syscfg0_OMAPL138.h: 
+../hamming_constants.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/dsplib.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap16/DSPF_blk_eswap16.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap16/c674/DSPF_blk_eswap16.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap32/DSPF_blk_eswap32.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap32/c674/DSPF_blk_eswap32.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap64/DSPF_blk_eswap64.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_blk_eswap64/c674/DSPF_blk_eswap64.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_fltoq15/DSPF_fltoq15.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_fltoq15/c674/DSPF_fltoq15.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_q15tofl/DSPF_q15tofl.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_q15tofl/c674/DSPF_q15tofl.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_autocor/DSPF_sp_autocor.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_autocor/c674/DSPF_sp_autocor.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_biquad/DSPF_sp_biquad.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_biquad/c674/DSPF_sp_biquad.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_bitrev_cplx/DSPF_sp_bitrev_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_bitrev_cplx/c674/DSPF_sp_bitrev_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_blk_move/DSPF_sp_blk_move.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_blk_move/c674/DSPF_sp_blk_move.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr2_dit/DSPF_sp_cfftr2_dit.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr2_dit/c674/DSPF_sp_cfftr2_dit.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr4_dif/DSPF_sp_cfftr4_dif.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_cfftr4_dif/c674/DSPF_sp_cfftr4_dif.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_convol/DSPF_sp_convol.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_convol/c674/DSPF_sp_convol.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotprod/DSPF_sp_dotprod.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotprod/c674/DSPF_sp_dotprod.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotp_cplx/DSPF_sp_dotp_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_dotp_cplx/c674/DSPF_sp_dotp_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fftSPxSP/DSPF_sp_fftSPxSP.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fftSPxSP/c674/DSPF_sp_fftSPxSP.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fircirc/DSPF_sp_fircirc.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fircirc/c674/DSPF_sp_fircirc.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_cplx/DSPF_sp_fir_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_cplx/c674/DSPF_sp_fir_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_gen/DSPF_sp_fir_gen.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_gen/c674/DSPF_sp_fir_gen.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_r2/DSPF_sp_fir_r2.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_fir_r2/c674/DSPF_sp_fir_r2.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_icfftr2_dif/DSPF_sp_icfftr2_dif.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_icfftr2_dif/c674/DSPF_sp_icfftr2_dif.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_ifftSPxSP/DSPF_sp_ifftSPxSP.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_ifftSPxSP/c674/DSPF_sp_ifftSPxSP.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iirlat/DSPF_sp_iirlat.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iirlat/c674/DSPF_sp_iirlat.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iir/DSPF_sp_iir.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_iir/c674/DSPF_sp_iir.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_lms/DSPF_sp_lms.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_lms/c674/DSPF_sp_lms.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul/DSPF_sp_mat_mul.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul/c674/DSPF_sp_mat_mul.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_cplx/DSPF_sp_mat_mul_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_cplx/c674/DSPF_sp_mat_mul_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm/DSPF_sp_mat_mul_gemm.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm/c674/DSPF_sp_mat_mul_gemm.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm_cplx/DSPF_sp_mat_mul_gemm_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_mul_gemm_cplx/c674/DSPF_sp_mat_mul_gemm_cplx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_trans/DSPF_sp_mat_trans.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_mat_trans/c674/DSPF_sp_mat_trans.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxidx/DSPF_sp_maxidx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxidx/c674/DSPF_sp_maxidx.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxval/DSPF_sp_maxval.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_maxval/c674/DSPF_sp_maxval.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minerr/DSPF_sp_minerr.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minerr/c674/DSPF_sp_minerr.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minval/DSPF_sp_minval.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_minval/c674/DSPF_sp_minval.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecmul/DSPF_sp_vecmul.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecmul/c674/DSPF_sp_vecmul.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecrecip/DSPF_sp_vecrecip.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecrecip/c674/DSPF_sp_vecrecip.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecsum_sq/DSPF_sp_vecsum_sq.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_vecsum_sq/c674/DSPF_sp_vecsum_sq.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_w_vec/DSPF_sp_w_vec.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSPF_sp_w_vec/c674/DSPF_sp_w_vec.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h16/DSP_fir_r8_h16.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h16/c674/DSP_fir_r8_h16.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h24/DSP_fir_r8_h24.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h24/c674/DSP_fir_r8_h24.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h8/DSP_fir_r8_h8.h: 
+C:/ti/dsplib_c674x_3_4_0_0/packages/ti/dsplib/src/DSP_fir_r8_h8/c674/DSP_fir_r8_h8.h: 
